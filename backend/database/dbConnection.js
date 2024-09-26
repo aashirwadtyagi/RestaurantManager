644731f9c294436928a06b57fc132497fadeeb1f
Aashirwad Tyagi <aashirwadtyagi940@gmail.com>
2024-09-26 13:07:20 +0530

    commit

---
+++ backend/database/dbConnection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const dbConnection = () => {
+  mongoose.connect(process.env.MONGO_URI, {
+      dbName: "restaurant",
+    })
+    .then(() => {
+      console.log("Connected to database!");
+    })
+    .catch((err) => {
+      console.log(`Some error occured while connecing to database: ${err}`);
+    });
+};
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Aashirwad</div>
+          <div className="right">
+            <p>A learning project</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p></p>
+          </div>
+          <div className="right">
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
