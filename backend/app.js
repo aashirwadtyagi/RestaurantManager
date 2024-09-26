219f687b54a8020b1aa3d4b03aae12dc011bb977
Aashirwad Tyagi <aashirwadtyagi940@gmail.com>
2024-09-26 15:19:20 +0530

    new

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1/reservation", reservationRouter);
+app.use("/api/v1/reservation/send", reservationRouter);
 app.get("/", (req, res, next)=>{return res.status(200).json({
   success: true,
   message: "HELLO WORLD AGAIN"
